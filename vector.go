f4d01f2c44d9419dae9b5c5c6ed9de3f4c6848fc
sjpau <theonlymoby@protonmail.com>
2023-05-29 23:54:54 +0300

    normalize

--- vector.go
+++ vector.go
@@ -18,3 +18,9 @@ func (v *Vector2D) Magnitude() float64 {
 func (v *Vector2D) Angle(v2 *Vector2D) float64 {
 	return math.Acos(v.Dot(v2) / (v.Magnitude() * v2.Magnitude()))
 }
+
+func (v *Vector2D) Normalize() {
+	length := v.Magnitude()
+	v.X /= length
+	v.Y /= length
+}
